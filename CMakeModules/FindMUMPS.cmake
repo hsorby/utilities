0214234a9bb0dd7bfe4f15df3689b2a4206832ae
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2014-11-24 15:25:57 +1300

    update on mumps find script

--- CMakeModules/FindMUMPS.cmake
+++ CMakeModules/FindMUMPS.cmake
@@ -8,10 +8,10 @@ IF(EXISTS ${MUMPS_DIR}/include/mumps_compat.h)
   SET(MUMPS_INCLUDES ${MUMPS_DIR})
   find_path (MUMPS_INCLUDE_DIR mumps_compat.h HINTS "${MUMPS_DIR}" PATH_SUFFIXES include NO_DEFAULT_PATH)
   list(APPEND MUMPS_INCLUDES ${MUMPS_INCLUDE_DIR})
-  FILE(GLOB MUMPS_LIBRARIES RELATIVE "${MUMPS_DIR}/lib" "${MUMPS_DIR}/lib/libmumps*.a")
-ELSE(EXISTS ${MUMPS_DIR}/include/mumps_compat.h)
+  FILE(GLOB MUMPS_LIBRARIES "${MUMPS_DIR}/lib/libmumps*.a") #RELATIVE "${MUMPS_DIR}/lib"
+ELSE()
   SET(MUMPS_FOUND NO)
-ENDIF(EXISTS ${MUMPS_DIR}/include/mumps_compat.h)
+ENDIF()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MUMPS DEFAULT_MSG MUMPS_LIBRARIES MUMPS_INCLUDES)
